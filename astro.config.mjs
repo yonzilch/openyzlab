ebdbc846637714270a58c693acbafacdce20951d
yonzilch <git@yonzilch.com>
2026-05-07 18:49:38 +0800

    feat(initial): use Astro + Starlight

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import catppuccin from "@catppuccin/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    server: {
+      allowedHosts: true,
+    },
+  },
+  integrations: [
+    starlight({
+      title: 'OpenYZLab',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/yonzilch/yzlab' }],
+      plugins: [
+        catppuccin({
+          dark: { flavor: "mocha", accent: "lavender" },
+          light: { flavor: "latte", accent: "teal" },
+        }),
+      ],
+      sidebar: [
+        {
+          label: 'Services',
+          autogenerate: { directory: 'services' },
+        },
+      ],
+    }),
+  ],
+});
